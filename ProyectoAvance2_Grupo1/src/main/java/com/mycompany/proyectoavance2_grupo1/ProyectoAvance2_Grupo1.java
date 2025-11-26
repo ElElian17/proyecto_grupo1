38a4f963206d74a76565ba97848ff825d7efbba2
Adrian Aguiar <adrianaguiar@Adrians-MacBook-Air.local>
2025-11-26 14:04:30 -0600

    prueba git adrian

--- ProyectoAvance2_Grupo1/src/main/java/com/mycompany/proyectoavance2_grupo1/ProyectoAvance2_Grupo1.java
+++ ProyectoAvance2_Grupo1/src/main/java/com/mycompany/proyectoavance2_grupo1/ProyectoAvance2_Grupo1.java
@@ -14,6 +14,9 @@ public class ProyectoAvance2_Grupo1 {
 
     public static void main(String[] args) {
         MenuPrincipal();
+        
+        System.out.println("Prueba para git");
+        
     }
     
     public static void MenuPrincipal(){
